c3816abd123c2b29f968ec6081d397bada594a63
Wafaa <Wafaabahgat>
2024-05-21 03:38:39 +0300

    h

--- src/components/section/Proposition_section.tsx
+++ src/components/section/Proposition_section.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import User_riew from "../User_riew";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import { Icons } from "../Icons";
-import { Reviews } from "./review/Reviews";
+import { Reviews } from "./reviews/Reviews";
 
 interface Proposition_sectionProps {}
 
